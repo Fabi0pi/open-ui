3f5701a028e09fdb108f11b049ea39cf1f9d4a2e
Fabio Piras <fabio.piras@spindox.it>
2024-05-06 14:42:38 +0200

    chore: added vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import dts from "vite-plugin-dts";
+
+/**
+ * DTS PLUGIN
+ * generates a file that contains the TYPES of all the funtions and components
+ * that we are exporting
+ */
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/components/index.tsx"),
+      name: "OpenUI",
+      fileName: "open-ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"], //dependency that we don't want to bring inside our package
+      output: {
+        globals: {
+          react: "Reat",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});
