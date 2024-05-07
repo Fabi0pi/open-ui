311669df1d552aa4995595d8cad4bd2a61510c20
Fabio Piras <fabio.piras@spindox.it>
2024-05-07 08:44:36 +0200

    fix: reindent

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,6 +1,7 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+
 /**   
  * Constructs a className string by merging multiple class values conditionally.
  * @param classes - An array of class values or strings representing class names.
