8b137e9a1a7c5eca32df5adee26402f1af6cb4dd
Fabio Piras <fabio.piras@spindox.it>
2024-05-12 13:03:28 +0200

    chore: build theme scheme

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+export const theme: Record<string, any> = {
+  colors: {
+    primary: {
+      light: "#42defe",
+      main: "#3182CE",
+      dark: "#0c3c5a",
+    },
+    secondary: {
+      light: "#e4f1f9",
+      main: "#9CA3AF",
+      dark: "#64748B",
+    },
+    error: "#EF4444",
+    success: "#10B981",
+  },
+  fontSizes: {
+    xl: 44,
+    lg: 32,
+    md: 24,
+    base: 18,
+    sm: 14,
+    xs: 10,
+  },
+};
