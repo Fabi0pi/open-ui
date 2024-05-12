d3e80fd55d17f990e1a84bb1e1bb06eda6649ff8
Fabio Piras <fabio.piras@spindox.it>
2024-05-12 15:40:51 +0200

    fix(text): variant type

--- src/components/Text/index.stories.tsx
+++ src/components/Text/index.stories.tsx
@@ -17,17 +17,26 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
     args: {
         children: "This is a normal text",
+        variant: "span"
     }
 }
 export const Emphasized: Story = {
     args: {
         children: "This is an emphasized text",
-        emphasis: true
+        emphasis: true,
+        variant: "p"
+
     }
 }
 export const Uppercase: Story = {
     args: {
         children: "This is an uppercase text",
         upperCase: true
+    }
+}
+export const Hero: Story = {
+    args: {
+        children: "This is an hero text",
+        variant: "h1"
     }
 }
